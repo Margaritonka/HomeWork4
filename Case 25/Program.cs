f1e1119d1d0df5bcca63a1082ce4c2345e8b8bf8
Margarita <bano4kaa@mail.ru>
2022-08-31 17:33:49 +0300

    case 25 done

--- Case 25/Program.cs
+++ Case 25/Program.cs
@@ -1,3 +1,14 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+double Degree()
+{
+Console.WriteLine("Введите целое число А ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число B ");
+int B = Convert.ToInt32(Console.ReadLine());
+double digr= Math.Pow(A,B);
+return digr;
+}
+Console.WriteLine(Degree());
