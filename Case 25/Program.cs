3ba93632e3d74dc08e4cf921864acd9da9461813
Margarita <bano4kaa@mail.ru>
2022-09-02 22:55:48 +0300

    change case 25

--- Case 25/Program.cs
+++ Case 25/Program.cs
@@ -1,14 +1,16 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
-
+//нужно было записать цикл, который используется в методе Match.Pow()
 double Degree()
 {
 Console.WriteLine("Введите целое число А ");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите целое число B ");
 int B = Convert.ToInt32(Console.ReadLine());
-double digr= Math.Pow(A,B);
+double digr = 1;
+for (int i = 0; i < B; i++) digr *= A;
+// double digr= Math.Pow(A,B);
 return digr;
 }
 Console.WriteLine(Degree());
